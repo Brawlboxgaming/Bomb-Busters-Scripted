d4da3f0833ae3c4012216dd08446863b1d2acc24
Brawlboxgaming <brawlboxgaming@duck.com>
2025-07-21 21:26:06 +0100

    Revert to test

--- SourceObjectsZone.lua
+++ SourceObjectsZone.lua
@@ -1,3 +1,9 @@
+function onLoad()
+    for _, object in ipairs(self.getObjects()) do
+        object.setInvisibleTo({"Blue", "Green", "Purple", "Red", "White", "Grey"})
+    end
+end
+
 function onObjectEnterScriptingZone(zone, object)
     if zone.getGUID() == self.getGUID() then
         object.setInvisibleTo({"Blue", "Green", "Purple", "Red", "White", "Grey"})
--- WireZones.lua
+++ WireZones.lua
@@ -1,5 +1,32 @@
+function onLoad()
+    guids = {
+        "735b22", -- Blue
+        "144eda", -- Green
+        "b45794", -- Purple
+        "cca9b9", -- Red
+        "3604a7", -- White
+        "000000"  -- Grey
+    }
+    colors = {
+        "Blue",
+        "Green",
+        "Purple",
+        "Red",
+        "White",
+        "Grey"
+    }
+    zoneColor = colors[self.getGUID()]
+    table.remove(colors, indexOf(guids, self.getGUID()))
+    for _, object in ipairs(self.getObjects()) do
+        if object.hasTag("Outer") then
+            object.setHiddenFrom({zoneColor})
+        else
+            object.setHiddenFrom(colors)
+        end
+    end
+end
+
 function onObjectEnterScriptingZone(zone, object)
-    log("onObjectEnterScriptingZone called for zone: " .. zone.getName() .. ", object: " .. object.getName())
     if zone.getGUID() == self.getGUID() then
         guids = {
             "735b22", -- Blue
