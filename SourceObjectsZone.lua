70c97d2615453bd4a5c62ed6c353ae3f3b75a970
Brawlboxgaming <brawlboxgaming@duck.com>
2025-07-21 21:11:07 +0100

    DEBUG: Revert

--- SourceObjectsZone.lua
+++ SourceObjectsZone.lua
@@ -1,18 +1,18 @@
 function onLoad()
-    for _, object in ipairs(self.getObjects()) do
+    zone = getObjectFromGUID("f55aed")
+    for _, object in ipairs(zone.getObjects()) do
         object.setInvisibleTo({"Blue", "Green", "Purple", "Red", "White", "Grey"})
     end
 end
 
 function onObjectEnterScriptingZone(zone, object)
-    if zone.getGUID() == self.getGUID() then
+    if zone.getGUID() == "f55aed" then
         object.setInvisibleTo({"Blue", "Green", "Purple", "Red", "White", "Grey"})
     end
 end
 
 function onObjectLeaveScriptingZone(zone, object)
-    log("onObjectLeaveScriptingZone called")
-    if zone.getGUID() == self.getGUID() then
+    if zone.getGUID() == "f55aed" then
         object.setInvisibleTo({})
     end
 end
