7a78735c341f118011213e08a513579ccbdeea95
Brawlboxgaming <brawlboxgaming@duck.com>
2025-07-21 21:16:05 +0100

    Optimisation

--- WireZones.lua
+++ WireZones.lua
@@ -1,31 +1,3 @@
-function onLoad()
-    guids = {
-        "735b22", -- Blue
-        "144eda", -- Green
-        "b45794", -- Purple
-        "cca9b9", -- Red
-        "3604a7", -- White
-        "000000"  -- Grey
-    }
-    colors = {
-        "Blue",
-        "Green",
-        "Purple",
-        "Red",
-        "White",
-        "Grey"
-    }
-    zoneColor = colors[self.getGUID()]
-    table.remove(colors, indexOf(guids, self.getGUID()))
-    for _, object in ipairs(self.getObjects()) do
-        if object.hasTag("Outer") then
-            object.setHiddenFrom({zoneColor})
-        else
-            object.setHiddenFrom(colors)
-        end
-    end
-end
-
 function onObjectEnterScriptingZone(zone, object)
     if zone.getGUID() == self.getGUID() then
         guids = {
