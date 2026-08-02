38d7e0f607458f10bf5077fd53e4ad873e1d7ccd
Brawlboxgaming <jghudson01@proton.me>
2026-08-02 20:46:24 +0100

    Fixed mission -15

--- Counter.lua
+++ Counter.lua
@@ -4120,12 +4120,12 @@ function spawnColouredWireTokens(missionNum)
             if missionConfig.redWires == "all" then
                 redCount = 11
             end
-        elseif missionConfig.redWires > 0 then
-            redCount = missionConfig.redWires
         elseif missionConfig.redWires.count then
             if missionConfig.redWires.count == "playerNum" then
                 redCount = playerNum
-        end
+            end
+        elseif missionConfig.redWires > 0 then
+            redCount = missionConfig.redWires
         end
     end
 
